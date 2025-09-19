002ad7da4049cb3bb83f70768f47b640f0e1e1ca
edgardo29 <i_edgardo@outlook.com>
2025-09-18 19:34:40 -0500

    feat(frontend): standings/weeklies tweaks; add matchDetails & teamPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import LeaguePage from "./pages/leaguePage/LeaguePage";
-import HealthTest from './pages/HealthTest';
+import HealthTest from "./pages/HealthTest";
+import TeamPage from "./pages/teamPage/TeamPage"; // NEW
+import MatchDetails from "./pages/matchDetails/MatchDetails";
 
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/league/:leagueId" element={<LeaguePage />} />
+        <Route path="/teams/:teamId" element={<TeamPage />} /> {/* NEW */}
+        <Route path="/match/:matchId" element={<MatchDetails />} />
         <Route path="/health-test" element={<HealthTest />} />
       </Routes>
     </Router>
